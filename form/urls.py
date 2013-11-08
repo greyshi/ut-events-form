c62c14f8fb52d848142a52c979c2498266989055
Arash Ghoreyshi <arashghoreyshi@gmail.com>
2013-11-08 00:24:39 -0800

    Add a couple of navigation links

--- form/urls.py
+++ form/urls.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', 'form.views.home', name='create'),
+    url(r'^$', 'form.views.home', name='home'),
     url(r'^create', 'form.views.create', name='create'),
     url(r'^events', 'form.views.events', name='events'),
     url(r'^categories', 'form.views.categories', name='categories'),
